c4db90390041a274fbb65261302818981ee114a6
Naveen <mnaveensmn@gmail.com>
2018-04-05 03:43:25 +0530

    Update Test.java

--- src/geekforgeeks/Test.java
+++ src/geekforgeeks/Test.java
@@ -9,6 +9,7 @@ public class Test {
         int myRandomNumber = 0;
         System.out.println(r.nextInt(9000-1000+1)+1000);
         System.out.println("added new code");
+        System.out.println("Pull request");
     }
   
 }
